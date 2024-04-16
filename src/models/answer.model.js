bf91489ac06eefd2aceb7af719f72b3a43732e0f
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-04-16 22:18:20 +0530

    Feat: Added Answer Model

---
+++ src/models/answer.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const answerSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    question_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Question",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Answer = mongoose.model("Answer", answerSchema);
+
+module.exports = Answer;
