ad3aec01339adf24a67cfededa2d79e2c0bb73f9
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-04-16 22:39:36 +0530

    Added Index.js file to models folder

---
+++ src/models/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  Answer: require("./answer.model"),
+  Comment: require("./comment.model"),
+  Question: require("./question.model"),
+  Topic: require("./topic.model"),
+  User: require("./user.model"),
+};
